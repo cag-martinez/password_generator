61aabec29c241b29088fa06c0906037e397d49a0
Christian Gomez Martinez <gomez9289@gmail.com>
2021-03-23 23:40:21 -0400

    generate password function

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+//DOM
 const resultPassword = document.getElementById("result");
 const copyPassword = document.getElementById("copy");
 const passwordLenght = document.getElementById("length");
@@ -8,6 +9,7 @@ const specialCharacters = document.getElementById("symbols");
 const generateButton = document.getElementById("generate");
 const form = document.getElementById("passwordForm")
 
+//for loop to increment until the highest value is achieved
 let lowToHighArray = (low, high) => {
     const array = [" "];
     for (let i = low; i <= high; i++){
@@ -16,19 +18,22 @@ let lowToHighArray = (low, high) => {
     return array;
 }
 
+//obtaining character codes from ASCII character table
 const LOWERCASE_CODES = lowToHighArray(97, 122);
 const UPPERCASE_CODES = lowToHighArray(65, 90);
 const NUMBER_CODES = lowToHighArray(48, 57);
 const CHARACTER_CODES = lowToHighArray (33, 47)
     .concat(lowToHighArray(58, 64))
     .concat(lowToHighArray(91, 96))
-    .concat(lowToHighArray(123, 126)) 
+    .concat(lowToHighArray(123, 126))
 
+    // testing to make sure i am getting the requested characters
     console.log(LOWERCASE_CODES);
     console.log(UPPERCASE_CODES);
     console.log(NUMBER_CODES);
     console.log(CHARACTER_CODES);
 
+//event listener, checking the options selected and creating the password
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         const characterLength = passwordLenght.nodeValue;
@@ -41,7 +46,27 @@ const CHARACTER_CODES = lowToHighArray (33, 47)
             includeNumbers,
             includeSymbols
         );
-        resultPassword.innerHTML = password
+        resultPassword.innerText = password
     });
+
+    //generating the password
+    //setting the default password to be lowercase if no option is checked
+    let generatePassword = (
+        characterLength,
+        includeUppercase,
+        includeNumbers,
+        includeSymbols
+    ) => {
+        let charCode = LOWERCASE_CODES;
+        if (includeUppercase) charCode = charCode.concat(UPPERCASE_CODES);
+        if (includeSymbols) charCode = charcode.concat(CHARACTER_CODES);
+        if (includeNumbers) charCode = charCode.concat(NUMBER_CODES);
+        const passwordChar = [];
+        for (let i = 0; i < characterLength; i++) {
+            const charCode = charCode[Math.floor(Math.random() * charCode.length)];
+            passwordChar.push(String.fromCharCode(charCode));
+        }
+        return passwordChar.join(" ");
+    };
 
     
